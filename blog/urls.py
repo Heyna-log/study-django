d7e83cc4ab143e3a3cc3d1d39239d8630528a955
nahinw <rhskgus048@naver.com>
2022-06-17 11:21:55 +0900

    CBV(Class Based View) - 블로그 리스트 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
         # uuid : UUID를 가리키며, 모든 문자가 소문자이어야 하고 대시(-)가 포함되어야 함. 이 converter는 uuid.UUID 객체를 리턴함.
         # path : 경로에 사용되는 슬래쉬(/)를 포함하는 문자열 리턴.
     path('<int:pk>/', views.single_post_page), # 'blog/pk(int형)/' url일때 blog.views.single_post_page 함수 호출
-    path('', views.index), # blog.views.index 함수 호출
+    # path('', views.index), # blog.views.index 함수 호출
+    path('', views.PostList.as_view()) # CBV
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-    # order_by('pk') : pk 순서대로
-    # order_by('-pk') : pk 역순서대로
+## CBV(Class Based View)
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    template_name = 'blog/index.html' # 기본 template name은 model명_list(post_list)이지만 이렇게 따로 지정해줄 수도 있음
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
+
+## FBV(Function Based View)
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#     # order_by('pk') : pk 순서대로
+#     # order_by('-pk') : pk 역순서대로
+#
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
 
 
 def single_post_page(request, pk):
