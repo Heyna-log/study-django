6e1a00a6b762f841ce3bbdf3db98f5174dde79a0
nahinw <rhskgus048@naver.com>
2022-07-26 11:27:26 +0900

    카테고리 만들기, Slug / blank=True, null=True 설명 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Category
 
-admin.site.register(Post) # admin 페이지에서 내가 만든 데이터베이스를 볼 수 있게 해주는..? 뭔지 정확히 찾아보기
+admin.site.register(Post) # admin 페이지에 만든 모델을 등록
+
+
+# admin custom
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)} # post를 추가할 경우 자동으로 카테고리 이름과 매핑되게 함(자동으로 카테고리 이름을 슬러그로 사용)
+
+
+admin.site.register(Category, CategoryAdmin)
--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+
+class Category(models.Model):
+    # 카테고리 이름
+    # unique=True : 중복 비허용
+    name = models.CharField(max_length=50, unique=True)
+
+    # 슬러그 : 이미 확보된 데이터를 사용하여 읽기 쉬운 형식의 url 생성. url이므로 중복되면 안됨!(unique=True)
+    # allow_unicode=True : 한글 사용 가능(특수기호 제외)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta: # 권한, 데이터베이스 이름, 단 복수 이름, 추상화, 순서 지정 등과 같은 모델에 대한 다양한 사항을 정의하는 데 사용
+        # verbose_name_plural : 모델에 대해 사람이 읽을 수 있는 복수형 이름을 정의하는 데 사용. Django의 기본 명명 규칙을 덮어씀. 이 이름은 관리자 패널 (/admin/)에도 반영됨.
+        verbose_name_plural = 'Categories'
+        # 다른 meta 옵션 참고 : https://www.delftstack.com/ko/howto/django/class-meta-in-django/
+
+
 class Post(models.Model):
     # 제목
     # CharField : 텍스트가 짧을 때
@@ -13,7 +32,7 @@ class Post(models.Model):
     content = models.TextField()
 
     # 이미지
-    # blank=True : null 허용
+    # blank=True : 필드가 폼(입력 양식)에서 빈 채로 저장되는 것을 허용. 장고 관리자(admin) 및 직접 정의한 폼에도 반영됨.
     # upload_to='blog/images/%y/%m/%d/' : blog/images/년/월/일 경로에 파일 저장
     head_image = models.ImageField(upload_to='blog/images/%y/%m/%d/', blank=True)
 
@@ -30,9 +49,13 @@ class Post(models.Model):
 
     # 작성자
     # 다대일 관계
+    # null=True : 필드의 값이 NULL(정보 없음)로 저장되는 것을 허용
     # on_delete=models.CASCADE : ForeignKeyField가 바라보는 값이 삭제될 때 ForeignKeyField를 포함하는 모델 인스턴스(row)도 삭제됨.
     # on_delete=models.SET_NULL : ForeignKeyField가 바라보는 값이 삭제될 때 ForeignKeyField값을 null로 바꾼다. (null=True일 때만 가능)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    # 카테고리
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self): # __str__ : 출력 시 문자열로 나옴
         return f'[{self.pk}] {self.title} - {self.author}' # f'{}' -> 포메팅
